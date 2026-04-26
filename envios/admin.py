a69f53f48abf59c780739bd1e27298e6b5b9707c
Luis Guerrero <luissguerreroo3@gmail.com>
2026-04-26 15:27:49 -0500

    QuerySets y managers implementados en sistema de encomiendas

--- envios/admin.py
+++ envios/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import Encomienda
+from .models import Encomienda, Empleado, HistorialEstado
+
 
 @admin.register(Encomienda)
 class EncomiendaAdmin(admin.ModelAdmin):
-    list_display = ('codigo', 'descripcion', 'peso_kg', 'estado', 'fecha_envio')
+    list_display = ('codigo', 'remitente', 'destinatario', 'estado', 'fecha_registro')
+
+
+@admin.register(Empleado)
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'apellidos', 'nombres')
+
+
+@admin.register(HistorialEstado)
+class HistorialEstadoAdmin(admin.ModelAdmin):
+    list_display = ('encomienda', 'estado_anterior', 'estado_nuevo', 'fecha_cambio')
